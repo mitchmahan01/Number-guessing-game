dd9a4fbcf9ec43d0bd6274e32b6544e3ee3b71e1
Mitch <mitchjmahan@gmail.com>
2023-05-15 23:21:10 -0500

    Update main.py
    Additional notes for code

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import random
 
 #Random number generator 
 n = random.randrange(1,10) 
-
+#User is prompted to guess # between 1-10
 guess = int(input("Enter any number between 1-10: "))
 
-while n !=guess: 
+while n !=guess: #if guess is too low, message below
 		if guess < n: 
 			print("Number is too low")
 			guess = int(input("Try another number: "))
-		else: 
+		else: #if guess is too high, message below
 			print("Number is too high")
 			guess = int(input("Enter a number again: "))
-
+#User has guessed correctly
 print("You guessed it right!")
